be6b2f0beff6e6df7f7ff254366d6e8d12bc1185
Oscar Figuerola <ojkarcete@gmail.com>
2011-12-22 13:20:13 +0100

    Lyria button removed, small aesthetic changes

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,6 @@
 /*****************************************************************************
 *	Chromium Extension to Display Lyrics for Current Song in Google Music 
-*	Beta from lyricwiki.org
+*	from lyricwiki.org
 *
 *   Copyright (C) 2011  Oscar Figuerola
 *
@@ -26,56 +26,19 @@
 var songName;
 var artistName;
 var albumName;
-var lyricsAreOn = 0;
 var timer;
 
+/**
+// Initialization point
 $(document).ready(function (){
-   // Modify player layout to add button  
-   var allHTMLTags = document.getElementsByTagName("*");
-   var playerNode;
-   var bgImageURL = chrome.extension.getURL("images/lyria-lyrics-button.png");
    
-   for (var i = 0; i < allHTMLTags.length; i++) {   
-       if (allHTMLTags[i].className == "player-left") {
-           playerNode = allHTMLTags[i];
-           playerNode.style.width = "505px";
-		   playerNode.id = "player-left";
-	   
-	       //add Image Button in player left
-	       $("#player-left").append("<div id=\"googlemusic_lyrics_nav\" class=\"googlemusic_lyrics_nav_off\" style=\"background-image:url(" + bgImageURL + 
-									");\"><span id=\"lyrics_on\" class=\"lyrics_state\">ON</span><span id=\"lyrics_off\" class=\"lyrics_state\">OFF</span></div>");
-		   $("#googlemusic_lyrics_nav").click(switchLyrics);
-           		   
-		   break;
-       }   
-	}
 });
+**/
 
-function switchLyrics()
-{
-	if (lyricsAreOn)
-	{
-		// Turn off lyrics
-		lyricsAreOn = 0;
-		clearTimeout(timer);
-		$('#googlemusic_lyrics_nav').addClass('googlemusic_lyrics_nav_off');
-		$('#googlemusic_lyrics_nav').removeClass('googlemusic_lyrics_nav_on');
-		
-		document.getElementById("lyrics_on").style.display = "none"; 
-		document.getElementById("lyrics_off").style.display = "block"; 
-	}
-	else
-	{	
-		// Turn on lyrics
-		lyricsAreOn = 1;
-		getLyrics();
-		$('#googlemusic_lyrics_nav').addClass('googlemusic_lyrics_nav_on');
-		$('#googlemusic_lyrics_nav').removeClass('googlemusic_lyrics_nav_off');	
-
-		document.getElementById("lyrics_off").style.display = "none"; 
-		document.getElementById("lyrics_on").style.display = "block"; 
-	}
-}
+// Add lyrics content area and start checking for sonf names
+$("#nav").append("<div class=\"nav-section-header\">LYRICS</div>");
+$("#nav").append("<div class=\"gm-lyrics\" id=\"gm-lyrics\">No song being played<br /><br /><br /></div>");
+getLyrics();
 
 function getLyrics(){  
    var	currentLyrics = "";
@@ -91,6 +54,7 @@ function getLyrics(){
    } 
    
    if (songName == $("#playerSongTitle .fade-out-content").text() && !thereIsError) {
+
 	 	// Keep timer on, but don't request again
 		timer = setTimeout("getLyrics()", 1000);
 		
@@ -101,32 +65,26 @@ function getLyrics(){
    artistName = $("#playerArtist .fade-out-content").text();
    albumName  = "";
    
-   if(songName == null){
-      //alert("Please Play a Song ");
+   if(songName == null) {
       event.preventDefault();
    }
-   else{
+   else {
 	  chrome.extension.sendRequest({song: songName,artist: artistName, album: albumName}, function(response) {  
 		  var lyricsElement; 
 			
 		  if (document.getElementById("gm-lyrics")) { 
 			 lyricsElement = document.getElementById("gm-lyrics"); 
-			 lyricsElement.innerHTML = response.lyrics;
+			 lyricsElement.innerHTML = response.lyrics + "<br /><br /><br /><br />";
 		  }
 		  else {
 			 $("#nav").append("<div class=\"nav-section-header\">LYRICS</div>");
-			 $("#nav").append("<div class=\"gm-lyrics\" id=\"gm-lyrics\">" + response.lyrics + "</div>");		
+			 $("#nav").append("<div class=\"gm-lyrics\" id=\"gm-lyrics\">" + response.lyrics + "<br /><br /><br /></div>");		
 		  }
 		  
 		  $(".gm-lyrics .rtMatcher").remove();     
 	   });
-   
    }
    
    // Keep timer on
    timer = setTimeout("getLyrics()", 1000);
 }
-
-
-
-
