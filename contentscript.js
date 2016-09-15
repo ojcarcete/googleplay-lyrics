bee47eaa1b3b8d97c0c7a77662830cedbbd82164
Oscar Figuerola <ojkarcete@gmail.com>
2016-09-15 19:50:18 +0200

    avoid overlapping songs queue and lyrics dialogs; recover automatically from missing lyrics dialog

--- contentscript.js
+++ contentscript.js
@@ -23,13 +23,15 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
-// Google Play html
+// Google Play HTML
 var SECTION_HEADER_ID = "playlists-header";
 var PLAYLISTS_DRAWER_BUTTON_ID = "playlist-drawer-button";
 var SONG_TITLE_CONTAINER_ID = "currently-playing-title";
 var ARTIST_CONTAINER_ID = "player-artist";
+var SONGS_QUEUE_BUTTON_ID = "queue";
+var SONGS_QUEUE_DIALOG_ID = "queue-overlay";
 
-// Custom html
+// Custom HTML
 var LYRICS_BUTTON_ID = "lyrics-button";
 var LYRICS_BUTTON_ICON = "av:library-books";
 
@@ -38,16 +40,22 @@ var LYRICS_DIALOG_ID = "scrolling";
 var LYRICS_HEADER_CLASS = "lyrics-header";
 var LYRICS_CONTAINER_ID = "lyrics";
 
+// Other variables
 var songName = null;
 var artistName = null;
 var firstLyricsLoaded = false;
 var lyricsContainerOffset = 0;
+var lastFetchedLyrics = "No lyrics to show yet";
 
+// Start fetching
 $(document).ready(function() {
 
   configureLyricsHtmlIfNeeded();
+  configureSongsQueueDialogButton();
   startFetchingLyrics();
 });
+
+// Lyrics Functions
 
 function startFetchingLyrics() {
 
@@ -63,78 +71,168 @@ function getLyrics() {
 
     var currentLyrics = lyricsContainer.html();
 
-    if (currentLyrics.search(/network error/i) >= 0) {
+    if (/network error/i.test(currentLyrics)) {
 
       thereIsError = true;
     }
   }
 
-  // Lyrics already retrieved
   if (lyricsAlreadyRetrievedForCurrentSong() && !thereIsError) {
 
     return;
   }
 
-  // Send new request to get the lyrics url
+  // Send a new request to get the lyrics url
   songName = $("#" + SONG_TITLE_CONTAINER_ID).text();
   artistName = $("#" + ARTIST_CONTAINER_ID).text();
 
   if (songName != null && songName != "") {
 
     chrome.extension.sendRequest({
-      song: songName,
-      artist: artistName
-    }, function(response) {
+        song: songName,
+        artist: artistName
+      },
+      function(response) {
 
-      console.log("Lyrics received");
+        // Process lyrics
+        lastFetchedLyrics = response.lyrics;
 
-      // Insert lyrics in page      
-      var lyricsHtml = response.lyrics;
+        lyricsContainer.html(lastFetchedLyrics);
+        lyricsContainer.children(".rtMatcher").remove();
 
-      lyricsContainer.html(lyricsHtml);
-      lyricsContainer.children(".rtMatcher").remove();
+        repositionLyricsDialog();
 
-      repositionLyricsDialog();
+        if (!firstLyricsLoaded) {
 
-      if (!firstLyricsLoaded) {
-
-        firstLyricsLoaded = true;
-
-        $("#" + LYRICS_BUTTON_ID).click();
-      }
-    });
+          firstLyricsLoaded = true;
+          openLyricsDialog();
+        }
+      });
 
     return;
   }
 }
+
+// Custom HTML Configuration Functions
 
 function configureLyricsHtmlIfNeeded() {
 
-  if (document.getElementById(LYRICS_DIALOG_ID)) {
+  addLyricsButtonIfNeeded();
+  addLyricsContainerIfNeeded();
+}
+
+function addLyricsButtonIfNeeded() {
+
+  if (lyricsButtonExists()) {
 
     return;
   }
 
-  addLyricsButton();
-  addLyricsContainer();
+  var LYRICS_BUTTON_HTML = "<paper-icon-button id=\"" + LYRICS_BUTTON_ID + "\" icon=\"" + LYRICS_BUTTON_ICON + "\" onclick=\"" + LYRICS_DIALOG_ID + ".toggle()\"></paper-icon-button>";
+
+  $("#" + PLAYLISTS_DRAWER_BUTTON_ID).before(LYRICS_BUTTON_HTML);
+
+  $("#" + LYRICS_BUTTON_ID).click(lyricsButtonPressed);
 }
 
-function addLyricsButton() {
+function addLyricsContainerIfNeeded() {
 
-  $("#" + PLAYLISTS_DRAWER_BUTTON_ID).before("<paper-icon-button id=\"" + LYRICS_BUTTON_ID + "\" icon=\"" + LYRICS_BUTTON_ICON + "\" onclick=\"" + LYRICS_DIALOG_ID + ".toggle()\"></paper-icon-button>");
-}
+  if (lyricsDialogExists()) {
 
-function addLyricsContainer() {
+    return;
+  }
 
   var LYRICS_HEADER_HTML = "<h2 id=\"" + SECTION_HEADER_ID + "\" class=\"" + LYRICS_HEADER_CLASS + "\">Lyrics</h2>";
-  var LYRICS_CONTAINER_HTML = "<div id=\"" + LYRICS_CONTAINER_ID + "\"><p>No lyrics to show yet</p></div>";
+  var LYRICS_CONTAINER_HTML = "<div id=\"" + LYRICS_CONTAINER_ID + "\"><p>" + lastFetchedLyrics + "</p></div>";
+  var LYRICS_DIALOG_HTML = "<paper-dialog onclick=\""+ LYRICS_DIALOG_ID + ".refit()\" id=\"" + LYRICS_DIALOG_ID + "\" horizontal-align=\"right\" vertical-align=\"bottom\" no-cancel-on-outside-click>" + LYRICS_HEADER_HTML + "<paper-dialog-scrollable>" + LYRICS_CONTAINER_HTML + "</paper-dialog-scrollable></paper-dialog>";
 
-  $("#" + LYRICS_DIALOG_CONTAINER_ID).append("<paper-dialog onclick=\"scrolling.refit()\" id=\"" + LYRICS_DIALOG_ID + "\" horizontal-align=\"right\" vertical-align=\"bottom\" no-cancel-on-outside-click>" + LYRICS_HEADER_HTML + "<paper-dialog-scrollable>" + LYRICS_CONTAINER_HTML + "</paper-dialog-scrollable></paper-dialog>");
+  $("#" + LYRICS_DIALOG_CONTAINER_ID).append(LYRICS_DIALOG_HTML);
+}
+
+function recoverFromMissingLyricsDialog() {
+
+  configureLyricsHtmlIfNeeded();
+  openLyricsDialog();
 }
 
 function repositionLyricsDialog() {
 
-  $("#" + LYRICS_DIALOG_ID).click();
+  $("#" + LYRICS_DIALOG_ID).click(); // triggers refit() call configured in dialog's HTML
+}
+
+// Google Play HTML Configuration Functions
+
+function configureSongsQueueDialogButton() {
+
+  $("#" + SONGS_QUEUE_BUTTON_ID).click(songsQueueButtonPressed);
+}
+
+// UI Actions Functions
+
+function lyricsButtonPressed() {
+
+  if (!lyricsDialogExists()) {
+
+    recoverFromMissingLyricsDialog();
+  } 
+  else if (isOpeningLyricsDialog()) {
+
+    closeSongsQueueDialog();
+  }
+}
+
+function openLyricsDialog() {
+
+  if (lyricsDialogExists() && !$("#" + LYRICS_DIALOG_ID).is(":visible")) {
+
+    $("#" + LYRICS_BUTTON_ID).click();
+  }
+}
+
+function closeLyricsDialog() {
+
+  if (lyricsDialogExists() && $("#" + LYRICS_DIALOG_ID).is(":visible")) {
+
+    $("#" + LYRICS_BUTTON_ID).click();
+  }
+}
+
+function songsQueueButtonPressed() {
+
+  if (isOpeningSongsQueueDialog()) {
+
+    closeLyricsDialog(); // Avoids overlapping dialogs
+  }
+}
+
+function closeSongsQueueDialog() {
+
+  if ($("#" + SONGS_QUEUE_DIALOG_ID).is(":visible")) {
+
+    $("#" + SONGS_QUEUE_BUTTON_ID).click();
+  }
+}
+
+// Helper Functions
+
+function lyricsButtonExists() {
+
+  return ($("#" + LYRICS_BUTTON_ID).length > 0);
+}
+
+function lyricsDialogExists() {
+
+  return ($("#" + LYRICS_DIALOG_ID).length > 0);
+}
+
+function isOpeningLyricsDialog() {
+
+  return !$("#" + LYRICS_DIALOG_ID).is(":visible");
+}
+
+function isOpeningSongsQueueDialog() {
+
+  return !$("#" + SONGS_QUEUE_DIALOG_ID).is(":visible");
 }
 
 function lyricsAlreadyRetrievedForCurrentSong() {
