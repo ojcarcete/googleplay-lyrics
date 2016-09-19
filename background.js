78686aafbd4ccab6353b27836b4e1447a3e74f8d
Oscar Figuerola <ojkarcete@gmail.com>
2016-09-19 22:53:56 +0200

    reformatted js code; fixed syntax error which prevented loading lyrics in some cases

--- background.js
+++ background.js
@@ -1,58 +1,72 @@
-
 // Return Message Function ptr
 var sendLyrics;
 var lyrics;
-var sourceURL;
+var sourceUrl;
 var status;
 var errorString;
 
 function loadLyrics(htmlResponse) {
 
   lyrics = $(htmlResponse).find('div[class="lyricbox"]').html();
-  sendLyrics({"status": "success" , "lyrics": lyrics , "sourceURL": sourceURL});
+  sendLyrics({
+    "status": "success",
+    "lyrics": lyrics,
+    "sourceURL": sourceUrl
+  });
 }
 
 // Extracts the lyric's link and sends a request to get them
 function getLyricsLink(xmlResponse) {
 
   var url = xmlResponse.getElementsByTagName('url');
-  sourceURL = url[0].textContent;
+  sourceUrl = url[0].textContent;
 
   try {
+    sourceUrl = decodeURIComponent(escape(decodeURI(sourceUrl)));
+  } catch (error) {
 
-    sourceURL = decodeURIComponent(escape(decodeURI(sourceURL)));
-  }
-  catch(err) {
-
-      console.error(error);
+    console.error(error);
   }
 
-  if(sourceURL.search(/edit/) >= 0) {
+  if (sourceUrl.search(/edit/) >= 0) {
 
-    sendLyrics({"status":"Not Found","lyrics":"Lyrics Not Available. </br> </br> you can " +
-      "add lyrics for this song at: </br> </br> <a href=\"" +
-      sourceURL + "\" target=\"_blank\">" + sourceURL + "</a>"   });
+    sendLyrics({
+      "status": "Not Found",
+      "lyrics": "Lyrics Not Available. </br> </br> you can " +
+        "add lyrics for this song at: </br> </br> <a href=\"" +
+        sourceUrl + "\" target=\"_blank\">" + sourceUrl + "</a>"
+    });
   }
+
+  console.log(sourceUrl);
 
   $.ajax({
-   url: sourceURL,
-   success: loadLyrics
+    url: sourceUrl,
+    success: loadLyrics
   });
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 
-    // Using lyrics.wikia.com REST api.
-    $.ajax({
-      url: "http://lyrics.wikia.com/api.php",
-      data: {artist: request.artist , song: request.song, fmt: "xml", func: "getSong"},
-      success: getLyricsLink
-    });
+  // Using lyrics.wikia.com REST api.
+  $.ajax({
+    url: "http://lyrics.wikia.com/api.php",
+    data: {
+      artist: request.artist,
+      song: request.song,
+      fmt: "xml",
+      func: "getSong"
+    },
+    success: getLyricsLink
+  });
 
-    sendLyrics = sendResponse;
+  sendLyrics = sendResponse;
 });
 
 $(document).ajaxError(function() {
 
-    sendLyrics({"status":"failure", "lyrics":"Network Error"});
+  sendLyrics({
+    "status": "failure",
+    "lyrics": "Network Error"
+  });
 });
